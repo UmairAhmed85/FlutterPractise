e6945a2e4cdb96b422ad08e59a40e5e5345347d5
Umair <uahmed@eddress.com>
2023-07-04 02:35:09 +0500

    Add widget padding
    (cherry picked from commit 71d82babb09e723f3009411c672f1fadba7a0d48)

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,9 @@ class MyApp extends StatelessWidget {
 
 class CarWidgetPage extends StatelessWidget {
   const CarWidgetPage({super.key, required this.title});
+
   final String title;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -31,23 +33,13 @@ class CarWidgetPage extends StatelessWidget {
         title: Text(title, style: const TextStyle(color: Colors.white)),
       ),
       body: const Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: <Widget>[
-          CarWidget(
-              'BMW',
-              'M3',
-              'https://media.ed.edmunds-media.com/bmw/m3/2018/oem/2018_bmw_m3_sedan_base_fq_oem_4_150.jpg'
-          ),
-          CarWidget(
-              'Honda',
-              'CIVIC',
-              'https://media.ed.edmunds-media.com/nissan/gt-r/2018/oem/2018_nissan_gt-r_coupe_nismo_fq_oem_1_150.jpg'
-          ),
-          CarWidget(
-              'Toyota',
-              'Corolla',
-              'https://media.ed.edmunds-media.com/nissan/sentra/2017/oem/2017_nissan_sentra_sedan_sr-turbo_fq_oem_4_150.jpg'
-          )
+          CarWidget('BMW', 'M3',
+              'https://media.ed.edmunds-media.com/bmw/m3/2018/oem/2018_bmw_m3_sedan_base_fq_oem_4_150.jpg'),
+          CarWidget('Honda', 'CIVIC',
+              'https://media.ed.edmunds-media.com/nissan/gt-r/2018/oem/2018_nissan_gt-r_coupe_nismo_fq_oem_1_150.jpg'),
+          CarWidget('Toyota', 'Corolla',
+              'https://media.ed.edmunds-media.com/nissan/sentra/2017/oem/2017_nissan_sentra_sedan_sr-turbo_fq_oem_4_150.jpg')
         ],
       ),
     );
@@ -58,12 +50,16 @@ class CarWidget extends StatelessWidget {
   final String _make;
   final String _model;
   final String _imageUrl;
+
   const CarWidget(this._make, this._model, this._imageUrl, {super.key});
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-        child: Column(
-      children: [Text(_make), Text(_model), Image.network(_imageUrl)],
-    ));
+    return Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Center(
+            child: Column(
+          children: [Text(_make), Text(_model), Image.network(_imageUrl)],
+        )));
   }
 }
