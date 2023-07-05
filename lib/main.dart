7b6590fe367b6429e7bca035fcdf87f0eed41329
Umair <uahmed@eddress.com>
2023-07-05 23:22:48 +0500

    Nav drawer with header

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
+
 void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  static const appTitle = 'Drawer Demo';
+
   @override
   Widget build(BuildContext context) {
-    const appTitle = 'Opacity Demo';
     return const MaterialApp(
       title: appTitle,
       home: MyHomePage(title: appTitle),
@@ -14,13 +16,8 @@ class MyApp extends StatelessWidget {
   }
 }
 
-// The StatefulWidget's job is to take data and create a State class.
-// In this case, the widget takes a title, and creates a _MyHomePageState.
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({
-    super.key,
-    required this.title,
-  });
+  const MyHomePage({super.key, required this.title});
 
   final String title;
 
@@ -28,42 +25,81 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-// The State class is responsible for two things: holding some data you can
-// update and building the UI using that data.
 class _MyHomePageState extends State<MyHomePage> {
-  // Whether the green box should be visible
-  bool _visible = true;
+  int _selectedIndex = 0;
+  static const TextStyle optionStyle = TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+  static const List<Widget> _widgetOptions = [
+    Text(
+      'Index 0: Page-1',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 1: Page-2',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 2: Page-3',
+      style: optionStyle,
+    ),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
+      appBar: AppBar(title: Text(widget.title)),
       body: Center(
-        child: AnimatedOpacity(
-          // If the widget is visible, animate to 0.0 (invisible).
-          // If the widget is hidden, animate to 1.0 (fully visible).
-          opacity: _visible ? 1.0 : 0.0,
-          duration: const Duration(milliseconds: 500),
-          // The green box must be a child of the AnimatedOpacity widget.
-          child: Container(
-            width: 200,
-            height: 200,
-            color: Colors.green,
-          ),
-        ),
+        child: _widgetOptions[_selectedIndex],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          // Call setState. This tells Flutter to rebuild the
-          // UI with the changes.
-          setState(() {
-            _visible = !_visible;
-          });
-        },
-        tooltip: 'Toggle Opacity',
-        child: const Icon(Icons.flip),
+      drawer: Drawer(
+        // Add a ListView to the drawer. This ensures the user can scroll
+        // through the options in the drawer if there isn't enough vertical
+        // space to fit everything.
+        child: ListView(
+          // Important: Remove any padding from the ListView.
+          padding: EdgeInsets.zero,
+          children: [
+            const DrawerHeader(
+              decoration: BoxDecoration(color: Colors.blue),
+              child: Text('Drawer Header'),
+            ),
+            ListTile(
+              title: const Text('Page-1'),
+              selected: _selectedIndex == 0,
+              onTap: () {
+                // Update the state of the app
+                _onItemTapped(0);
+                // Then close the drawer
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: const Text('Page-2'),
+              selected: _selectedIndex == 1,
+              onTap: () {
+                // Update the state of the app
+                _onItemTapped(1);
+                // Then close the drawer
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: const Text('Page-3'),
+              selected: _selectedIndex == 2,
+              onTap: () {
+                // Update the state of the app
+                _onItemTapped(2);
+                // Then close the drawer
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
