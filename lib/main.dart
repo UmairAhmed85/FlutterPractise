dd0a20b5e47e93aec492f4c2ca0a9920af833d47
Umair <uahmed@eddress.com>
2023-07-09 14:06:57 +0500

    Send custom data to next screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,109 +1,83 @@
 import 'package:flutter/material.dart';
 
+class User{
+  final String name;
+  final String occupation;
+
+  const User(this.name, this.occupation);
+}
+
 void main() {
+  const userList = [
+    User("User - 1", "Software Engineer"),
+    User("User - 2", "Android Engineer"),
+    User("User - 3", "IOS Engineer"),
+    User("User - 4", "Electrical Engineer"),
+    User("User - 5", "Mechanical Engineer")
+  ];
   runApp(
     const MaterialApp(
-      title: 'Returning Data',
-      home: HomePage(),
+      title: 'Demo',
+      home: UsersScreen(
+        users: userList,
+      ),
     ),
   );
 }
 
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+class UsersScreen extends StatelessWidget {
+  const UsersScreen({super.key, required this.users});
+
+  final List<User> users;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Demo'),
+        title: const Text('Users'),
       ),
-      body: const Center(
-        child: HomePageButton(),
+      body: ListView.builder(
+        itemCount: users.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text(users[index].name),
+            subtitle: Text(users[index].occupation),
+
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => const UserDetailScreen(),
+                  // Pass the arguments as part of the RouteSettings. The
+                  // DetailScreen reads the arguments from these settings.
+                  settings: RouteSettings(
+                    arguments: users[index],
+                  ),
+                ),
+              );
+            },
+          );
+        },
       ),
     );
   }
 }
 
-class HomePageButton extends StatefulWidget {
-  const HomePageButton({super.key});
-
-  @override
-  State<HomePageButton> createState() => _HomePageButtonState();
-}
-
-class _HomePageButtonState extends State<HomePageButton> {
-  @override
-  Widget build(BuildContext context) {
-    return ElevatedButton(
-      onPressed: () {
-        _navigateAndDisplaySelection(context);
-      },
-      child: const Text('Go to Detail page & send results back!'),
-    );
-  }
-
-  // A method that launches the SelectionScreen and awaits the result from
-  // Navigator.pop.
-  Future<void> _navigateAndDisplaySelection(BuildContext context) async {
-    // Navigator.push returns a Future that completes after calling
-    // Navigator.pop on the Selection Screen.
-    final result = await Navigator.push(
-      context,
-      MaterialPageRoute(builder: (context) => const DetailPage()),
-    );
-
-    // When a BuildContext is used from a StatefulWidget, the mounted property
-    // must be checked after an asynchronous gap.
-    if (!mounted) return;
-
-    // After the Selection Screen returns a result, hide any previous snackbars
-    // and show the new result.
-    ScaffoldMessenger.of(context)
-      ..removeCurrentSnackBar()
-      ..showSnackBar(SnackBar(content: Text('$result')));
-  }
-}
-
-class DetailPage extends StatelessWidget {
-  const DetailPage({super.key});
+class UserDetailScreen extends StatelessWidget {
+  // In the constructor, require a User.
+  const UserDetailScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final user = ModalRoute.of(context)!.settings.arguments as User;
+    // Use the User to create the UI.
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Detail Page'),
+        title: Text(user.name),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Padding(
-              padding: EdgeInsets.all(8),
-                child: Text('Select any option to send it back on Homepage'),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8),
-              child: ElevatedButton(
-                onPressed: () {
-                  // Close the screen and return "Yep!" as the result.
-                  Navigator.pop(context, 'Yep!');
-                },
-                child: const Text('Yep!'),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8),
-              child: ElevatedButton(
-                onPressed: () {
-                  // Close the screen and return "Nope." as the result.
-                  Navigator.pop(context, 'Nope.');
-                },
-                child: const Text('Nope.'),
-              ),
-            )
-          ],
-        ),
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Text("${user.name} is ${user.occupation}"),
       ),
     );
   }
